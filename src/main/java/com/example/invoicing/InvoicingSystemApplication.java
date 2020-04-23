b0a422290be9867aae414b2b13dfe230a56a866c
yhwang <1051646698@qq.com>
2020-04-23 13:15:30 +0800

    初始项目

---
+++ src/main/java/com/example/invoicing/InvoicingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.invoicing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoicingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/invoicing/InvoicingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.invoicing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
